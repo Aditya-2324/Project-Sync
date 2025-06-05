20e6849a2b92670912ca53a181a6d05b2e29c679
Aditya-2324 <adityapharande2@gmail.com>
2025-06-05 19:56:18 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "project-sync-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/client.js",
+  "/style.css",
+  "/manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
